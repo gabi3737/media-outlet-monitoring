6d7c00f1b803a6a823909c1cd1e5953f1019bec3
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-08 15:36:22 +0100

    Added tests and adjusted function

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -2,3 +2,110 @@
 Test for the transform.py file
 """
 import pytest
+from datetime import datetime, timezone
+import pandas as pd
+
+from transform import clean_publication_time, clean_string_columns
+
+
+@pytest.fixture
+def valid_dataframe():
+    return pd.DataFrame({
+        "published": [
+            "Mon, 07 Sep 2026 10:30:00 +0000",
+            "Thu, 27 Aug 2026 14:01:00 GMT",
+        ],
+        "title": [
+            " test TITLE onE ",
+            "  TEST title tWo",
+        ],
+        "content": [
+            " test one  ",
+            " test  two   ",
+        ],
+        "author": [
+            " Lily Hay Newman, Matt Burgess, Dhruv Mehrotra   ",
+            " mmarshall@venturebeat.com (Matt Marshall)",
+        ],
+        "tags": [
+            "Gear, Gear / Gear News and Events, artificial intelligence, Say More",
+            " Machine Learning / AI ",
+        ]
+    })
+
+
+@pytest.fixture
+def empty_dataframe():
+    return pd.DataFrame({
+        "published": [None],
+        "title": [None],
+        "content": [None],
+        "author": [None],
+        "tags": [None]
+    })
+
+
+@pytest.fixture
+def invalid_dataframe():
+    return pd.DataFrame({
+        "published": ["Hello"],
+        "title": [123],
+        "content": [123],
+        "author": [123],
+        "tags": [123]
+    })
+
+
+def test_clean_publication_time_valid(valid_dataframe):
+    result = clean_publication_time(valid_dataframe)
+
+    assert result["published"].iloc[0] == pd.Timestamp(
+        "2026-09-07 10:30:00+00:00"
+    )
+    assert result["published"].iloc[1] == pd.Timestamp(
+        "2026-08-27 14:01:00+00:00"
+    )
+
+
+def test_clean_publication_time_empty(empty_dataframe):
+    result = clean_publication_time(empty_dataframe)
+    assert pd.isna(result["published"].iloc[0])
+
+
+def test_clean_publication_time_invalid(invalid_dataframe):
+    result = clean_publication_time(invalid_dataframe)
+    assert pd.isna(result["published"].iloc[0])
+
+
+def test_clean_string_columns_valid(valid_dataframe):
+    result = clean_string_columns(valid_dataframe)
+    assert result["title"].iloc[0] == "test TITLE onE"
+    assert result["title"].iloc[1] == "TEST title tWo"
+
+    assert result["content"].iloc[0] == "test one"
+    assert result["content"].iloc[1] == "test  two"
+
+    assert result["author"].iloc[0] == "Lily Hay Newman, Matt Burgess, Dhruv Mehrotra"
+    assert result["author"].iloc[1] == "Mmarshall@Venturebeat.Com (Matt Marshall)"
+
+    assert set(result["tags"].iloc[0]) == {'gear',
+                                           'gear news and events',
+                                           'artificial intelligence',
+                                           'say more'}
+    assert set(result["tags"].iloc[1]) == {'machine learning', 'ai'}
+
+
+def test_clean_string_columns_empty(empty_dataframe):
+    result = clean_string_columns(empty_dataframe)
+    assert pd.isna(result["title"].iloc[0])
+    assert pd.isna(result["content"].iloc[0])
+    assert pd.isna(result["author"].iloc[0])
+    assert result["tags"].iloc[0] == []
+
+
+def test_clean_string_columns_invalid(invalid_dataframe):
+    result = clean_string_columns(invalid_dataframe)
+    assert result["title"].iloc[0] == "123"
+    assert result["content"].iloc[0] == "123"
+    assert result["author"].iloc[0] == "123"
+    assert result["tags"].iloc[0] == ['123']
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -2,13 +2,17 @@
 The transform aspect of the pipeline
 """
 import logging
-import pandas as pd
 from datetime import datetime, timezone
+import pandas as pd
 
 
 def clean_publication_time(data: pd.DataFrame) -> pd.DataFrame:
     """Returns a dataframe with a cleaned publication time"""
     logging.info("Cleaning the published column")
+    data['published'] = data['published'].str.replace(
+        r"\s+\S+$", "", regex=True
+    )
+
     data['published'] = pd.to_datetime(
         data['published'], errors='coerce', utc=True)
 
@@ -22,15 +26,16 @@ def clean_publication_time(data: pd.DataFrame) -> pd.DataFrame:
 def clean_string_columns(data: pd.DataFrame) -> pd.DataFrame:
     """Returns a dataframe where string columns are all cleaned"""
     logging.info("Cleaning all string columns")
-    data['title'] = data['title'].str.strip()
-    data['content'] = data['content'].str.strip()
-    data['author'] = data['author'].str.strip().str.capitalize()
+    data['title'] = data['title'].astype(str).str.strip()
+    data['content'] = data['content'].astype(str).str.strip()
+    data['author'] = data['author'].astype(str).str.strip().str.title()
     data["tags"] = (
         data["tags"]
+        .astype(str)
         .str.strip()
         .str.lower()
         .str.split(r", | / ")
-        .apply(lambda x: list(set(x)))
+        .apply(lambda x: list(set(x)) if isinstance(x, list) else [])
     )
 
     return data
@@ -40,8 +45,8 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
 
-    data = pd.read_csv("pipeline/data/wired_ai_feed.csv")
-    # data = pd.read_csv("pipeline/data/venturebeat_ai_feed.csv")
+    # data = pd.read_csv("pipeline/data/wired_ai_feed.csv")
+    data = pd.read_csv("pipeline/data/venturebeat_ai_feed.csv")
 
     data = clean_publication_time(data)
     data = clean_string_columns(data)
