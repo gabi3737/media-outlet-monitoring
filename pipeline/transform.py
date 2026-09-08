9b3be9e577b5bc4efcade00800f60fcec0ee04d3
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-08 13:43:20 +0100

    Cleaned string columns

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -3,17 +3,27 @@ The transform aspect of the pipeline
 """
 import logging
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def clean_publication_time(data: pd.DataFrame) -> pd.DataFrame:
     """Returns a dataframe with a cleaned publication time"""
-    data['publication_time'] = pd.to_datetime(
-        data['publication_time'], errors='coerce')
+    data['published'] = pd.to_datetime(
+        data['published'], errors='coerce', utc=True)
 
-    data["publication_time"] = data["publication_time"].where(
-        data["publication_time"] < datetime.now()
+    data["published"] = data["published"].where(
+        data["published"] < datetime.now(timezone.utc)
     )
+
+    return data
+
+
+def clean_string_columns(data: pd.DataFrame) -> pd.DataFrame:
+    """Returns a dataframe where string columns are all cleaned"""
+    data['title'] = data['title'].str.strip()
+    data['content'] = data['content'].str.strip()
+    data['author'] = data['author'].str.strip().str.capitalize()
+    data["tags"] = data["tags"].str.strip().str.lower().str.split(', ')
 
     return data
 
@@ -21,3 +31,10 @@ def clean_publication_time(data: pd.DataFrame) -> pd.DataFrame:
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
+
+    data = pd.read_csv("pipeline/data/wired_ai_feed.csv")
+    # data = pd.read_csv("pipeline/data/venturebeat_ai_feed.csv")
+
+    data = clean_publication_time(data)
+    data = clean_string_columns(data)
+    print(data['tags'])
