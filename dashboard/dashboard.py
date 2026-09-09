a6a3188649d6fa4dee58c41cf32d6ab8547ce318
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-09 17:55:17 +0100

    Added function signatures for dashboard metrics

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,32 @@ import logging
 import streamlit as st
 import altair as alt
 
+from data import (
+    get_total_articles,
+    get_average_sentiment,
+    get_company_mention_count
+)
+
 
 if __name__ == "__main__":
 
     st.markdown("# Otranto Development: AI Media Analysis")
+
+    # Metrics
+    st.markdown("## Overview")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(
+            "Total Articles",
+            f"{get_total_articles('data')}"
+        )
+    with col2:
+        st.metric(
+            "Average Sentiment",
+            f"{get_average_sentiment('data')}"
+        )
+    with col3:
+        st.metric(
+            "Company Mention Count",
+            f"{get_company_mention_count('data')}"
+        )
--- dashboard/data.py
+++ dashboard/data.py
@@ -3,3 +3,18 @@ Functions that manipulate the dataframe for the visualisations
 """
 import logging
 import pandas as pd
+
+
+def get_total_articles(data: pd.DataFrame) -> int:
+    """Returns the total amount of articles"""
+    return 1
+
+
+def get_average_sentiment(data: pd.DataFrame) -> float:
+    """Returns the average sentiment"""
+    return 1
+
+
+def get_company_mention_count(data: pd.DataFrame) -> int:
+    """Returns the count of companies mentioned"""
+    return 1
