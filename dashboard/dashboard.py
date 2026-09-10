2a1295eb41dfa9ad941de96ad3f74566e2ebfd67
Javed <ja9611705@gmail.com>
2026-09-10 10:54:18 +0100

    Added charts for top ten companies most mentioned

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,8 +11,9 @@ from data_functions import (
     get_clean_data,
     get_average_sentiment,
     get_company_mention_count,
-    get_average_sentiment_by_company
-
+    get_average_sentiment_by_company,
+    get_average_sentiment_for_top_companies,
+    get_daily_sentiment,
 )
 
 
@@ -36,13 +37,37 @@ def load_data() -> pd.DataFrame:
 
 
 def company_chart_by_sentiment(data: pd.DataFrame) -> alt.Chart:
-    """Generate a sentiment chart grouped by company."""
-    return alt.Chart(get_average_sentiment_by_company(data).sort_values(by="sentiment", ascending=False).head(10)).mark_bar().encode(
-        x='companies',
-        y='sentiment',
+    """Generate a bar chart for top ten companies with greatest sentiment."""
+    return alt.Chart(get_average_sentiment_by_company(data).head(10)).mark_bar().encode(
+        x=alt.X('companies', title='Company'),
+        y=alt.Y('sentiment', title='Average Sentiment'),
         tooltip=['companies', 'sentiment'],
         color=alt.Color('companies', scale=alt.Scale(
             scheme='tableau10'), legend=None)
+    )
+
+
+def sentiment_chart_by_company(data: pd.DataFrame) -> alt.Chart:
+    """Generate a bar chart for top ten companies by sentiment"""
+    return alt.Chart(get_average_sentiment_for_top_companies(data).head(10)).mark_bar().encode(
+        x=alt.X('companies', title='Company'),
+        y=alt.Y('mean_sentiment', title='Average Sentiment'),
+        tooltip=['companies', 'mean_sentiment', 'count'],
+        color=alt.Color('companies', scale=alt.Scale(
+            scheme='tableau10'), legend=None)
+    )
+
+
+def top_ten_companies_daily_sentiment(data: pd.DataFrame) -> alt.Chart:
+    """Generate a line chart for top ten companies' daily sentiment."""
+    top_companies = get_average_sentiment_for_top_companies(data).head(10)[
+        'companies']
+    daily_sentiment = get_daily_sentiment(data, top_companies)
+    return alt.Chart(daily_sentiment).mark_line().encode(
+        x=alt.X('published', title='Date'),
+        y=alt.Y('sentiment', title='Average Sentiment'),
+        color=alt.Color('companies', scale=alt.Scale(scheme='tableau10')),
+        tooltip=['companies', 'published', 'sentiment']
     )
 
 
@@ -75,8 +100,17 @@ if __name__ == "__main__":
         )
 
     st.markdown("## Sentiment Analysis")
+    col1, col2 = st.columns(2)
 
-    st.markdown("#### Top 10 Companies with Highest Sentiment")
-    st.altair_chart(company_chart_by_sentiment(data), use_container_width=True)
+    with col1:
+        st.markdown("#### Top 10 Companies with Highest Sentiment")
+        st.altair_chart(company_chart_by_sentiment(data), width='stretch')
+
+    with col2:
+        st.markdown("#### Top 10 Companies by Sentiment")
+        st.altair_chart(sentiment_chart_by_company(data), width='stretch')
+
+    st.markdown("## Top 10 Companies Sentiment Over Time")
+    st.altair_chart(top_ten_companies_daily_sentiment(data), width='stretch')
 
     st.dataframe(data)
